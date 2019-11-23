186d089ae27f431e3bd94cbb2cae0b848d4cd8c1
Maxim Konovalov <maksik852002@gmail.com>
2019-11-23 15:59:43 +0600

    Добавить компонент Order #2

---
+++ src/components/Order/Order.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Order = props => {
+  if(props.goods) 
+
+  return (
+    <div className="d-flex my-2 align-items-center">
+      <div className="col-5">
+        <p>{props.name}</p>
+      </div>
+      <div className="col-5">
+        <p className="d-inline mr-4">x {props.count}</p>
+        <p className='m-0 d-inline'>{props.price} kgs </p>
+      </div>
+      <div className="col-2"><button onClick={props.remove} className="close">x</button></div>
+    </div>
+  )
+}
+
+export default Order
--- src/container/App.js
+++ src/container/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import '../bootstrap.min.css';
 import Goods from '../components/Goods/Goods'
+import Order from '../components/Order/Order'
 
 import { FaHamburger } from 'react-icons/fa';
 import { GiHamburger } from 'react-icons/gi';
@@ -22,37 +23,62 @@ const GOODS = [
 class App extends Component {
   state = { 
     goods: [
- 
+      {name: 'Hamburger', price:80, count: 0, total:0, id: 'gd1'},
+      {name: 'Cheeseburger', price:90, count: 0, total:0, id: 'gd2'},
+      {name: 'Fries', price:45, count: 0, total:0, id: 'gd3'},
+      {name: 'Coffe', price:70, count: 0, total:0, id: 'gd4'},
+      {name: 'Tea', price:50, count: 0, total:0, id: 'gd5'},
+      {name: 'Cola', price:40, count: 0, total:0, id: 'gd6'},
     ],
-   
+    totalPrice: 0,
   };
 
-  render = () => (
-    <div className="container">
-      <div className="row mt-3 text-center">
-        <div className="col-12 col-md-6 mb-3">
-          <h5>Order Details:</h5>
-          <div className="border border-secondary rounded p-2">
+  render = () => {
+    let id = [];
+    return (
+      <div className="container">
+        <div className="row mt-3 text-center">
+          <div className="col-12 col-md-6 mb-3">
+            <h5>Order Details:</h5>
+            <div className="border border-secondary rounded p-2">
+              {
+                this.state.goods.map(good => {
+                  id.push(good.id) 
+                  return (
+                    good.count > 0 && <Order
+                      key={good.id}
+                      name = {good.name}
+                      price = {good.price} 
+                      count = {good.count}
+                      goods = {this.state.goods}
+                      remove = {()=> this.removeGood(good.id)}
+                    />
+                  )
+                })
+              }
+              <p>Total price: {this.state.totalPrice}</p>
+            </div>
           </div>
-        </div>
-        <div className="col-12 col-md-6 mb-3">
-          <h5>Add items:</h5>
-          <div className="border border-secondary rounded p-2"> 
-            {
-              GOODS.map((good, i) => (
-                <Goods
-                key = {i}
-                name = {good.name}
-                price = {good.price}
-                image = {good.image}
-                />
-              ))
-            }
+          <div className="col-12 col-md-6 mb-3">
+            <h5>Add items:</h5>
+            <div className="border border-secondary rounded p-2"> 
+              {
+                GOODS.map((good, i) => (
+                  <Goods
+                  key = {id[i]}
+                  name = {good.name}
+                  price = {good.price}
+                  image = {good.image}
+                  click= {()=> this.addGood(id[i])}
+                  />
+                ))
+              }
+            </div>
           </div>
-        </div>
-      </div>  
-    </div>
-  )
+        </div>  
+      </div>
+    )
+  }
 };
 
 export default App;
