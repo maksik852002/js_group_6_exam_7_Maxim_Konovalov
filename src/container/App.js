853c0db1db9075f73421a84047a3e90927f69160
Maxim Konovalov <maksik852002@gmail.com>
2019-11-23 12:23:49 +0600

    Компонент Goods #1

--- src/container/App.js
+++ src/container/App.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react';
 import './App.css';
-import './bootstrap.min.css';
+import '../bootstrap.min.css';
+import Goods from '../components/Goods/Goods'
+
+import { FaHamburger } from 'react-icons/fa';
+import { GiHamburger } from 'react-icons/gi';
+import { GiPotato } from 'react-icons/gi';
+import { DiCoffeescript } from 'react-icons/di';
+import { FaCoffee } from 'react-icons/fa';
+import { MdLocalDrink } from 'react-icons/md';
 
 const GOODS = [
-  {name: 'Hamburger', price:80, image: <GiLindenLeaf/>},
-  {name: 'Cheeseburger', price:90, image: <GiCheeseWedge/>},
-  {name: 'Fries', price:45, image: <GiMeat/>},
-  {name: 'Coffe', price:70, image: <FaBacon/>},
-  {name: 'Tea', price:50, image: <FaBacon/>},
-  {name: 'Cola', price:40, image: <FaBacon/>},
+  {name: 'Hamburger', price:80, image: <FaHamburger/>},
+  {name: 'Cheeseburger', price:90, image: <GiHamburger/>},
+  {name: 'Fries', price:45, image: <GiPotato/>},
+  {name: 'Coffe', price:70, image: <DiCoffeescript/>},
+  {name: 'Tea', price:50, image: <FaCoffee/>},
+  {name: 'Cola', price:40, image: <MdLocalDrink/>},
 ];
 
 class App extends Component {
@@ -30,6 +38,16 @@ class App extends Component {
         <div className="col-12 col-md-6 mb-3">
           <h5>Add items:</h5>
           <div className="border border-secondary rounded p-2"> 
+            {
+              GOODS.map((good, i) => (
+                <Goods
+                key = {i}
+                name = {good.name}
+                price = {good.price}
+                image = {good.image}
+                />
+              ))
+            }
           </div>
         </div>
       </div>  
